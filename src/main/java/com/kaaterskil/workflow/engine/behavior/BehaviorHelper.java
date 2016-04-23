e9a2cdfd37f7aeca18e3d655ec5c1c5c681c50e9
Blair Caple <bcaple@akamai.com>
2016-04-23 11:53:27 -0400

    Set multi-instance behavior handler in helper

--- src/main/java/com/kaaterskil/workflow/engine/behavior/BehaviorHelper.java
+++ src/main/java/com/kaaterskil/workflow/engine/behavior/BehaviorHelper.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.kaaterskil.workflow.bpm.common.FlowNode;
+import com.kaaterskil.workflow.bpm.common.activity.Activity;
+import com.kaaterskil.workflow.bpm.common.activity.MultiInstanceLoopCharacteristics;
 import com.kaaterskil.workflow.engine.behavior.handler.BehaviorHandler;
 import com.kaaterskil.workflow.engine.context.Context;
 import com.kaaterskil.workflow.engine.delegate.ActivityBehavior;
@@ -25,13 +27,20 @@ public class BehaviorHelper {
         }
     }
 
-    public ActivityBehavior getBehavior(FlowNode element) {
+    public ActivityBehavior getBehavior(FlowNode flowNode) {
         final ActivityBehaviorFactory factory = Context.getProcessEngineService()
                 .getActivityBehaviorFactory();
 
-        final BehaviorHandler handler = behaviorHandlers.get(element.getClass());
+        if (flowNode instanceof Activity) {
+            final Activity activity = (Activity) flowNode;
+            if (activity.getLoopCharacteristics() != null) {
+                return createMultiInstanceActivityBehavior(factory, activity);
+            }
+        }
+
+        final BehaviorHandler handler = behaviorHandlers.get(flowNode.getClass());
         if (handler != null) {
-            return handler.getBehavior(factory, element);
+            return handler.getBehavior(factory, flowNode);
         }
         return null;
     }
@@ -46,6 +55,44 @@ public class BehaviorHelper {
 
     private void performOutgoingBehavior(Token token, boolean checkConditions) {
         Context.getCommandContext().getWorkflow().addOutgoingSequenceFlow(token, checkConditions);
+    }
+
+    private MultiInstanceActivityBehavior createMultiInstanceActivityBehavior(
+            ActivityBehaviorFactory factory, Activity activity) {
+        final MultiInstanceLoopCharacteristics loopCharacteristics = (MultiInstanceLoopCharacteristics) activity
+                .getLoopCharacteristics();
+
+        final BaseActivityBehavior innerActivityBehavior = (BaseActivityBehavior) getBehavior(
+                activity);
+        MultiInstanceActivityBehavior behavior = null;
+        if (loopCharacteristics.isSequential()) {
+            behavior = factory.createSequentialMultiInstanceBehavior(activity,
+                    innerActivityBehavior);
+        } else {
+            behavior = factory.createParallelMultiInstanceBehavior(activity, innerActivityBehavior);
+        }
+
+        if (loopCharacteristics.getLoopCardinality() != null) {
+            behavior.setLoopCardinalityExpression(loopCharacteristics.getLoopCardinality());
+        }
+
+        if (loopCharacteristics.getCompletionCondition() != null) {
+            behavior.setCompletionConditionExpression(loopCharacteristics.getCompletionCondition());
+        }
+
+        // The name of a process variable which is a collection. For each item in the collection an
+        // instance will be created.
+        if (loopCharacteristics.getLoopDataInputRef() != null) {
+            behavior.setCollectionVariable(loopCharacteristics.getLoopDataInputRef());
+        }
+
+        // The specific item of the collection.
+        if (loopCharacteristics.getInputDataItem() != null) {
+            behavior.setCollectionElementVariable(
+                    loopCharacteristics.getInputDataItem().getItemSubjectRef());
+        }
+
+        return behavior;
     }
 
     /*---------- Getter/Setters ----------*/
--- src/main/java/com/kaaterskil/workflow/engine/parser/handler/AbstractActivityParseHandler.java
+++ src/main/java/com/kaaterskil/workflow/engine/parser/handler/AbstractActivityParseHandler.java
@@ -4,10 +4,8 @@ import com.kaaterskil.workflow.bpm.common.FlowNode;
 import com.kaaterskil.workflow.bpm.common.activity.Activity;
 import com.kaaterskil.workflow.bpm.common.activity.MultiInstanceLoopCharacteristics;
 import com.kaaterskil.workflow.bpm.foundation.BaseElement;
-import com.kaaterskil.workflow.engine.behavior.BaseActivityBehavior;
-import com.kaaterskil.workflow.engine.behavior.BehaviorHelper;
-import com.kaaterskil.workflow.engine.behavior.MultiInstanceActivityBehavior;
-import com.kaaterskil.workflow.engine.context.Context;
+import com.kaaterskil.workflow.engine.behavior.ParallelMultiInstanceBehavior;
+import com.kaaterskil.workflow.engine.behavior.SequentialMultiInstanceBehavior;
 import com.kaaterskil.workflow.engine.parser.BpmParser;
 
 public abstract class AbstractActivityParseHandler<T extends FlowNode>
@@ -28,40 +26,10 @@ public abstract class AbstractActivityParseHandler<T extends FlowNode>
     private void createMultiInstanceLoopCharacteristics(BpmParser parser, Activity activity) {
         final MultiInstanceLoopCharacteristics loopCharacteristics = (MultiInstanceLoopCharacteristics) activity
                 .getLoopCharacteristics();
-
-        final BehaviorHelper behaviorHelper = Context.getProcessEngineService()
-                .getBehaviorHelper();
-        final BaseActivityBehavior innerActivityBehavior = (BaseActivityBehavior) behaviorHelper
-                .getBehavior(activity);
-
-        MultiInstanceActivityBehavior behavior = null;
         if (loopCharacteristics.isSequential()) {
-            behavior = parser.getActivityBehaviorFactory()
-                    .createSequentialMultiInstanceBehavior(activity, innerActivityBehavior);
+            activity.setBehavior(SequentialMultiInstanceBehavior.class.getCanonicalName());
         } else {
-            behavior = parser.getActivityBehaviorFactory()
-                    .createParallelMultiInstanceBehavior(activity, innerActivityBehavior);
-        }
-        activity.setBehavior(behavior.getClass().getCanonicalName());
-
-        if (loopCharacteristics.getLoopCardinality() != null) {
-            behavior.setLoopCardinalityExpression(loopCharacteristics.getLoopCardinality());
-        }
-
-        if (loopCharacteristics.getCompletionCondition() != null) {
-            behavior.setCompletionConditionExpression(loopCharacteristics.getCompletionCondition());
-        }
-
-        // The name of a process variable which is a collection. For each item in the collection an
-        // instance will be created.
-        if (loopCharacteristics.getLoopDataInputRef() != null) {
-            behavior.setCollectionVariable(loopCharacteristics.getLoopDataInputRef());
-        }
-
-        // The specific item of the collection.
-        if (loopCharacteristics.getInputDataItem() != null) {
-            behavior.setCollectionElementVariable(
-                    loopCharacteristics.getInputDataItem().getItemSubjectRef());
+            activity.setBehavior(ParallelMultiInstanceBehavior.class.getCanonicalName());
         }
     }
 }
